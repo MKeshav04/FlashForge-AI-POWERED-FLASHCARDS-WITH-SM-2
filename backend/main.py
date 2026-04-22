1c5f884502c7aca21072c16940414686d121c3d3
MKeshav04 <keshav04mishra@gmail.com>
2026-04-22 18:34:21 +0530

    Quick fix

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,10 @@ app = FastAPI(title="Cuemath Flashcard API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], 
+    allow_origins=[
+    "http://localhost:5173", 
+    "https://flash-forge.netlify.app"
+], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
